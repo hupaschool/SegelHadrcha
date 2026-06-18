f57aaea896303bd16d0ced31563b80ab64a00b81
hupaschool <schoolhupa@gmail.com>
2026-06-18 05:03:27 +0300

    finished animation, waiting for more info

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import whiteStripe from "./assets/firstPage/whiteStripe.png";
+import firstPage from "./assets/firstPage/frontPage.png";
+import secondPage from "./assets/secondPage.png";
+import thirdPage from "./assets/thirdPage.png";
+import fourthPage from "./assets/fourthPage.png";
+import fifthPage from "./assets/fifthPage.png";
+import openingText from "./assets/firstPage/openingText.png";
+import darkRec from "./assets/firstPage/darkRectangle.png"
+
+
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div>
+      <div className="all-pages">
+        <div className="reveal page" id="first-page">
+            <img src={firstPage} />
+            <img src={whiteStripe} className="white-stripe"/>
+            <img src={openingText} className="opening-text"/>
+            <div className="lower-banner">
+              <img src={darkRec} className="dark-rec"/>
+            </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <div className="page" id="second-page">
+            <img src={secondPage} />
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <div className="page" id="third-page">
+            <img src={thirdPage} />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        </div>
+        <div className="page" id="fourth-page">
+            <img src={fourthPage} />
+
+        </div>
+        <div className="page" id="fifth-page">
+            <img src={fifthPage} />
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
