3eb48d4e70b40d32f0c3c5a9fe0d35a156756617
hupaschool <schoolhupa@gmail.com>
2026-06-18 13:42:15 +0300

    fixed the pages and logos

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import fifthPage from "./assets/fifthPageNew.png";
 import openingText from "./assets/firstPage/openingText.png";
 import darkRec from "./assets/firstPage/darkRectangle.png";
 import date from "./assets/firstPage/date.png";
+import schoolLogo from "./assets/schoolLogo.svg";
+import airForceLogo from "./assets/airForceLogo.svg";
 import { useEffect, useState } from "react";
 
 import "./App.css";
@@ -22,6 +24,10 @@ function App() {
     <div>
       <div className="all-pages">
         <div className="reveal page" id="first-page">
+          <div className="logos">
+            <img src={schoolLogo} className="logo" id="school-logo"/>
+            <img src={airForceLogo} className="logo" id="air-force-logo"/>
+          </div>
           <img src={firstPage} />
           <img src={whiteStripe} className={animate ? "white-stripe stripe-animation" : "white-stripe"} />
           <img src={openingText} className={animate ? "opening-text text-animation" : "opening-text"} />
@@ -31,15 +37,31 @@ function App() {
           </div>
         </div>
         <div className="page" id="second-page">
+          <div className="logos">
+            <img src={schoolLogo} className="logo" id="school-logo"/>
+            <img src={airForceLogo} className="logo" id="air-force-logo"/>
+          </div>
           <img src={secondPage} />
         </div>
         <div className="page" id="third-page">
+          <div className="logos">
+            <img src={schoolLogo} className="logo" id="school-logo"/>
+            <img src={airForceLogo} className="logo" id="air-force-logo"/>
+          </div>
           <img src={thirdPage} />
         </div>
         <div className="page" id="fourth-page">
+          <div className="logos">
+            <img src={schoolLogo} className="logo" id="school-logo"/>
+            <img src={airForceLogo} className="logo" id="air-force-logo"/>
+          </div>
           <img src={fourthPage} />
         </div>
         <div className="page" id="fifth-page">
+          <div className="logos">
+            <img src={schoolLogo} className="logo" id="school-logo"/>
+            <img src={airForceLogo} className="logo" id="air-force-logo"/>
+          </div>
           <img src={fifthPage} />
         </div>
       </div>
